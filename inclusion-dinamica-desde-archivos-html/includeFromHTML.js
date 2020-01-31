35d68dfa73b56d5b5ac726aa90ebec2c5bcd2d44
us-ability <60501974+us-ability@users.noreply.github.com>
2020-01-31 12:24:32 +0100

    Add files via upload
    practicas 1, 2 y 3

---
+++ inclusion-dinamica-desde-archivos-html/includeFromHTML.js
@@ -0,0 +1,36 @@
+
+function includeFromHTML(selector, file) {
+    // Inicializamos petición de Ajax
+    var xhttp = new XMLHttpRequest();
+
+    // Recuperamos el elemento contenedor
+    var trg = document.querySelector(selector);
+
+    // Si no lo encontramos mostramos una mensaje de error
+    if(!trg) alert("El elemento contnedor no se encuentra");
+
+    xhttp.onreadystatechange = function () {
+        // Si la petición está completada y lista
+        if (this.readyState == 4) {
+            if (this.status == 200) { 
+                // Y tuvo éxito
+                trg.innerHTML = this.responseText; 
+
+            } else if (this.status == 404) { 
+                // Y el archivo no lo encuentra o no existe
+                trg.innerHTML = "Archivo no encontrado."; 
+
+            } else {
+                // Y no sabemos que ocurrió
+                trg.innerHTML = "Error no controlado."; 
+            }
+        }
+    }
+
+    // Realizamos la petición
+    xhttp.open("GET", file, true);
+    xhttp.send();
+
+    return;
+}
+
